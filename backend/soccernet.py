839ef167cd9a6df5bd0bb7f262e56f04e40ae833
MohammedAlkhrashi <32398807+MohammedAlkhrashi@users.noreply.github.com>
2025-04-24 21:55:24 +0300

    soccernet download

--- backend/soccernet.py
+++ backend/soccernet.py
@@ -2,8 +2,8 @@
 import SoccerNet
 from SoccerNet.Downloader import SoccerNetDownloader
 mySoccerNetDownloader=SoccerNetDownloader(LocalDirectory="soccernet/")
-mySoccerNetDownloader.password = input("password: ")
-# mySoccerNetDownloader.downloadGames(files=["1_720p.mkv", "2_720p.mkv"], split=["train","valid","test","challenge"])
-mySoccerNetDownloader.downloadGames(files=["1_224p.mkv", "2_224p.mkv"], split=["valid","test","challenge"])
+mySoccerNetDownloader.password = "password here!"
+# %
+mySoccerNetDownloader.downloadDataTask(task="tracking-2023", split=["test"])
 
 # %%
